0940ce05c18c5204b5f11c64c66609845a30b66a
Jefferson Ramos <jrangelramos@gmail.com>
2022-08-30 18:19:09 -0300

    allow fetch of unadvertised object

--- git-repo.sh
+++ git-repo.sh
@@ -7,6 +7,9 @@ if [[ "$1" == "create" && ! -f "$REPO_PATH" ]]; then
   mkdir -p "$REPO_PATH"
   cd "$REPO_PATH"
   git init --bare
+  git config uploadpack.allowTipSha1InWant true
+  git config uploadpack.allowTipSha1InWant true
+  git config uploadpack.allowTipSha1InWant true
   chown -Rf git:git "$REPO_PATH"
   chmod -R g+w "$REPO_PATH"
   echo "created"
